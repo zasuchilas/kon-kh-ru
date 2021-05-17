5a5906ab8ce44751d01c1105227d0660b2f38c33
kon-kh <k.e.hitrov@yandex.ru>
2021-05-17 18:25:58 +0300

    Добавлены манифест, иконки, яндекс.метрика

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,5 +46,37 @@ module.exports = {
         },
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `kon-kh.ru`,
+        short_name: `kon-kh`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#FF0000`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    // {
+    //   resolve: `gatsby-plugin-yandex-metrika`,
+    //   options: {
+    //     // The ID of yandex metrika.
+    //     trackingId: 71337565,
+    //     // Enabled a webvisor. The default value is `false`.
+    //     webvisor: true,
+    //     // Enables tracking a hash in URL. The default value is `false`.
+    //     trackHash: true,
+    //     // Defines where to place the tracking script - `false` means before body (slower loading, more hits)
+    //     // and `true` means after the body (faster loading, less hits). The default value is `false`.
+    //     afterBody: true,
+    //     // Use `defer` attribute of metrika script. If set to `false` - script will be loaded with `async` attribute.
+    //     // Async enables earlier loading of the metrika but it can negatively affect page loading speed. The default value is `false`.
+    //     defer: false,
+    //   },
+    // },
   ],
 };
